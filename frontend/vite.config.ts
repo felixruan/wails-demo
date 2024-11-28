996d438a9f1a35080963747704d8a34ae1b61fca
阮杰辉 <ruanjiehui@qq.com>
2024-11-28 14:03:37 +0800

    引入前端js

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,11 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Icons from 'unplugin-icons/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 
+const rootPath = new URL('.', import.meta.url).pathname
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -14,11 +16,21 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
+    Icons(),
   ],
   resolve: {
     alias: {
-      '@': '/src'
+      '@': rootPath + 'src',
+      stores: rootPath + 'src/stores',
+      wailsjs: rootPath + 'wailsjs',
     }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
   },
   server: {
     port: 18080
